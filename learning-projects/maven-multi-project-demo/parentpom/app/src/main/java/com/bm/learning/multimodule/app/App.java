e66f7e98f4409243569adb2d86f9eafd48ab9d35
dev-blue-mountain <dev.blue.mountain@gmail.com>
2023-08-15 18:26:10 -0700

    add Product class and move the App class under a different package

--- learning-projects/maven-multi-project-demo/parentpom/app/src/main/java/com/bm/learning/multimodule/app/App.java
+++ learning-projects/maven-multi-project-demo/parentpom/app/src/main/java/com/bm/learning/multimodule/app/App.java
@@ -1,5 +1,6 @@
 package com.bm.learning.multimodule.app;
 
+import com.bm.learning.multimodule.model.Product;
 import com.bm.learning.multimodule.dto.User;
 import com.bm.learning.multimodule.helpers.StringUtils;
 
@@ -8,6 +9,9 @@ public class App{
 		User user = new User("karim");
 		String userUpperCase = StringUtils.toUpper(user.getFirstName());
 		System.out.println(userUpperCase);
+
+		Product p = new Product("mouse");
+		System.out.println(p.getProductName());
 
 	}
 }
---
+++ learning-projects/maven-multi-project-demo/parentpom/app/src/main/java/com/bm/learning/multimodule/model/Product.java
@@ -0,0 +1,13 @@
+package com.bm.learning.multimodule.model;
+
+public class Product {
+    private final String productName;
+
+     public Product(String name){
+         productName = name;
+     }
+
+    public String getProductName() {
+        return productName;
+    }
+}
