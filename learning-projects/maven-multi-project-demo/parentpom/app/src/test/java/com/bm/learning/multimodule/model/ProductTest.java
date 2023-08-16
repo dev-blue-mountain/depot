c36a751c983523a1931872674b6c57fe1f5e6491
dev-blue-mountain <dev.blue.mountain@gmail.com>
2023-08-16 16:34:01 -0700

    add a test that has stream usages

--- learning-projects/maven-multi-project-demo/parentpom/app/src/test/java/com/bm/learning/multimodule/model/ProductTest.java
+++ learning-projects/maven-multi-project-demo/parentpom/app/src/test/java/com/bm/learning/multimodule/model/ProductTest.java
@@ -2,12 +2,25 @@ package com.bm.learning.multimodule.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.Stream;
+
 class ProductTest {
     @Test
     void getProduct(){
         Product p = new Product("keyboard");
         System.out.println(p.getProductName());
 
+    }
+
+    @Test
+    void streamTest(){
+        String[] strArray = { "x", "y", "xyz", "abc"};
+        Object[] outputStrArray = Stream.of(strArray)
+                .map(item-> item.toUpperCase())
+                .filter(elem-> elem.length() ==1)
+                .sorted()
+                .toArray();
+        System.out.println(outputStrArray);
     }
 
 
