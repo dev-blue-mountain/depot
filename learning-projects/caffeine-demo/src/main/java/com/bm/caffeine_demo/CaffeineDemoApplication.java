855d116f2e3e8edf18a46aab8e6d0f27e09d2f29
dev-blue-mountain <dev.blue.mountain@gmail.com>
2024-10-30 16:50:17 -0700

    adding the caffeine demo project

---
+++ learning-projects/caffeine-demo/src/main/java/com/bm/caffeine_demo/CaffeineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bm.caffeine_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffeineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaffeineDemoApplication.class, args);
+	}
+
+}
---
+++ learning-projects/caffeine-demo/src/test/java/com/bm/caffeine_demo/CaffeineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bm.caffeine_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaffeineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
